19399a75617f428f43113df1b0d59f6bc6c6843c
sarthakwebsite26012000-dot <sarthakwebsite26012000@gmail.com>
2025-12-05 14:08:11 +0530

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,30 @@
+// Constants for TravelSaathi Application
+const TRAVEL_MODES = {
+    FLIGHT: 'flight',
+    TRAIN: 'train',
+    BUS: 'bus'
+};
+
+const PROVIDERS = {
+    MAKEMYTRIP: 'makemytrip',
+    GOIBIBO: 'goibibo',
+    IRCTC: 'irctc',
+    REDBUS: 'redbus'
+};
+
+const STORAGE_KEYS = {
+    USER_TOKEN: 'userToken',
+    USER_DATA: 'userData',
+    SEARCH_HISTORY: 'searchHistory',
+    PREFERENCES: 'userPreferences'
+};
+
+const API_ENDPOINTS = {
+    SEARCH: '/api/search',
+    ALERTS: '/api/alerts',
+    USER: '/api/user'
+};
+
+const MAX_SEARCH_HISTORY = 50;
+const DEFAULT_CURRENCY = 'INR';
+const APP_VERSION = '1.0.0';
